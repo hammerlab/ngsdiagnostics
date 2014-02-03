9323c05a1e7dcb010040df983d99f9cea9dbb00d
Neal Sidhwaney <nealsid+github@gmail.com>
2014-02-02 23:55:26 -0500

    Update UI to take into account user selection - code needs to be cleaned up, this is just a checkpoint

--- perfdash/main.py
+++ perfdash/main.py
@@ -1,10 +1,41 @@
 from flask import Flask
+from flask import request
 from flask import render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
+
 import json
 import StringIO
 import csv
 
 app = Flask(__name__)
+# db = SQLAlchemy(app)
+
+# # Create your models here.
+# class RunDescription(db.Model):
+#     __tablename__ = 'Performance_Runs'
+#     timestamp = db.Column(db.DateTime)
+#     args = db.Column(db.String(10000))
+
+#     def __unicode__(self):
+#         return str(self.timestamp) + ":" + self.args
+
+# class PerfStep(db.Model):
+#     __tablename__ = 'Performance Run Steps'
+#     step_name = db.Column(db.String(max_length=100))
+    
+#     def __unicode__(self):
+#         return self.step_name
+
+# class PerfMeasurement(db.Model):
+#     step = db.Column(models.ForeignKey(PerfStep)
+#     perf_run = models.ForeignKey(RunDescription)
+#     step_time = models.IntegerField()
+
+#     def __str__(self):
+#         return self.step.step_name + ":" + str(self.step_time) + "@" + str(self.perf_run.timestamp)
+
 
 @app.route('/perfdash/data')
 def data():
@@ -18,9 +49,16 @@ def data():
      ['MarkDuplicates','19983','9165','8141','32254','3540'],
      ['BaseRecalibrator','1142','1485','986','103','1064'],
      ['SortSam','20286','15122','14641','14882','8370']]
+    requested_steps = request.args.get('requested_steps')
+    new_data = data
+    if requested_steps:
+        new_data = [data[0]]
+        new_data.extend(filter(lambda step_data: step_data[0] in requested_steps.split(","), data[1:]))
+    app.logger.debug("new data: %s", str(new_data))
+    app.logger.debug("Returning data for following steps: %s", ",".join([x[0] for x in new_data]))
     output = StringIO.StringIO()
     csv_writer = csv.writer(output, delimiter='	')
-    for x in data:
+    for x in new_data:
         csv_writer.writerow(x)
     return output.getvalue()
 
