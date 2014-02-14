23a6882b0484adc7953b022ffb8335dc34ae3168
Neal Sidhwaney <nealsid+github@gmail.com>
2014-02-14 13:58:33 -0500

    Remove Django app

--- ngsdash/perfdash/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from perfdash import views
-
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^data$', views.data_as_tsv, name='data_as_tsv')
-)
