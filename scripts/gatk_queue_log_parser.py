212c53de9c0fe396a2b5a1f86140e9a95f3dabef
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-03-16 02:07:56 -0400

    Bring new parser to parity with the old parser.

--- scripts/gatk_queue_log_parser.py
+++ scripts/gatk_queue_log_parser.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 import argparse
-import datetime
+from  datetime import datetime as dt, time, timedelta as td
 import hashlib
 import logging
 import re
@@ -21,33 +21,48 @@ STEPS = ['bwa mem', 'SortSam', 'RealignerTargetCreator', 'IndelRealigner',
 #
 # Extract
 #
-START_TIME_RE= re.compile('Date/Time: (\\d{4}/\\d{2}/\\d{2} \\d{2}:\\d{2}:\\d{2})')
+START_TIME_RE = re.compile('Date/Time: (\\d{4}/\\d{2}/\\d{2} \\d{2}:\\d{2}:\\d{2})')
 def handle_start_time_line(match, run_info):
     start_time_string = match.group(1)
-    start_time = datetime.datetime.strptime(start_time_string, '%Y/%m/%d %H:%M:%S')
+    start_time = dt.strptime(start_time_string, '%Y/%m/%d %H:%M:%S')
     run_info['start_time'] = start_time
 
 
-ARGS_RE= re.compile('Program Args: (.*)$')
+ARGS_RE = re.compile('Program Args: (.*)$')
 def handle_args_line(match, run_info):
     run_info['args'] = match.group(1)
+
+
+# TODO(hammer): pick this up in a more general way
+# TODO(hammer): handle if more than one distinct sample is found
+SAMPLE_RE= re.compile("SM:([^\\\\]+)")
+def handle_sample_line(match, run_info):
+    run_info['sample'] = match.group(1)
 
 
 FUNCTION_EDGE_RE = re.compile('INFO\\s+(\\d{2}):(\\d{2}):(\\d{2}),(\\d+) FunctionEdge\\s+-\\s+([^:]+):\\s+(.*)')
 def handle_function_edge_line(match, run_info):
     h, m, s, ms, edge_type, body = match.groups()
 
+    # TODO(hammer): handle runs > 24 hours
+    line_t = time(int(h), int(m), int(s))
+    start_dt = run_info.get('start_time')
+    if not start_dt:
+        raise Exception('Found a FunctionEdge line before start time could be determined.')
+    line_d = start_dt.date() if line_t > start_dt.time() else start_dt.date() + td(days=1)
+    line_dt = dt.combine(line_d, line_t)
+
     step = [step for step in STEPS if step in body]
     if len(step) == 0:
         return
     elif len(step) > 1:
-        raise Exception("Matched multiple steps (%s) in %s" % (','.join(step), body))
+        raise Exception('Matched multiple steps (%s) in %s' % (','.join(step), body))
     else:
         step = step[0]
 
     if edge_type in ('Starting', 'Done'):
         body_hash = hashlib.md5(body.encode()).hexdigest()
-        run_info['steps'][step].append((body_hash, edge_type, ':'.join([h, m, s])))
+        run_info['steps'][step].append((body_hash, edge_type, line_dt))
 
 
 def parse_file(filename):
@@ -55,6 +70,7 @@ def parse_file(filename):
     run_info['steps'] = defaultdict(list)
     line_actions = [(START_TIME_RE, handle_start_time_line),
                     (ARGS_RE, handle_args_line),
+                    (SAMPLE_RE, handle_sample_line),
                     (FUNCTION_EDGE_RE, handle_function_edge_line)]
 
     with open(filename, 'r') as infile:
@@ -67,12 +83,51 @@ def parse_file(filename):
 
 
 #
+# Transform
+#
+
+# TODO(hammer): do this in the database (ELT!)
+def get_avg_step_times(run_info):
+    avg_times = {}
+    for step_name, step_info in run_info['steps'].items():
+        ss = sorted(step_info)
+        # TODO(hammer): add some error checking to this very cute comprehension
+        step_times = [(x[2] - y[2]).seconds for (x, y) in zip(ss[::2], ss[1::2])]
+        logging.info("Step times for step %s: %s" % (step_name, step_times))
+        avg_times[step_name] = sum(step_times) / len(step_times)
+    return avg_times
+
+
+#
 # Load
 #
 
 # TODO(hammer): generalize to handle sqlite3 and psycopg2
 def get_conn(db):
     return sqlite3.connect(db)
+
+
+# TODO(hammer): make consistent with get_step_id
+def get_run_id(conn, run_info):
+    c = conn.cursor()
+    # NB datetime.timestamp() is only in Python >= 3.3
+    timestamp = int(run_info['start_time'].timestamp())
+    args = run_info['args']
+    sample = run_info['sample']
+    try:
+        c.execute("""\
+        INSERT INTO run_timing_metadata (run_timestamp, run_args, sample)
+        VALUES      (?, ?, ?)""", (timestamp, args, sample))
+        conn.commit()
+    except sqlite3.IntegrityError as err:
+        logging.info("Run metadata (args, timestamp, sample) already existed.")
+
+    result = c.execute("""\
+    SELECT rowid from run_timing_metadata
+    WHERE  run_timestamp = ? and run_args = ? and sample = ?
+    """, (timestamp, args, sample))
+    row_id = result.fetchone()[0]
+    return row_id
 
 
 def get_step_id(conn, step_name):
@@ -97,6 +152,16 @@ def insert_step_time(conn, run_id, step_name, step_time):
     c.execute("INSERT INTO perf_measurements (stepid, run_id, step_time) VALUES (?, ?, ?)",
               (step_id, run_id, step_time))
     conn.commit()
+    logging.info('Inserted new row into perf_measurements: (%s, %s, %s)' %
+                 (step_id, run_id, step_time))
+
+
+def store_results(db, run_info, avg_step_times):
+    conn = get_conn(db)
+    run_id = get_run_id(conn, run_info)
+    for step_name, avg_step_time in avg_step_times.items():
+      step_id = get_step_id(conn, step_name)
+      insert_step_time(conn, run_id, step_name, avg_step_time)
 
 
 #
@@ -110,11 +175,19 @@ def main(argv):
     args = parser.parse_args()
 
     if args.verbose: logging.basicConfig(level=logging.DEBUG)
-    persist_to_db = True if args.db else False
+    store_in_db = True if args.db else False
 
     for logfile in args.logfiles:
+        # Extract
         run_info = parse_file(logfile)
-        logging.info('Run info parsed from %s: %s' % (logfile, run_info))
+        logging.info('Run info parsed from %s' % logfile)
+
+        # Transform
+        avg_step_times = get_avg_step_times(run_info)
+        logging.info('Average step times: %s' % (avg_step_times))
+
+        # Load
+        if store_in_db: store_results(args.db, run_info, avg_step_times)
 
 
 if __name__ == '__main__':
